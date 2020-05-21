aca6117153bd62489617952ecf0c31be12d5bccc
Eric Delord <edelord@gmail.com>
2020-05-21 17:04:24 +0200

    simplified Kml component

--- src/components/Kml.svelte
+++ src/components/Kml.svelte
@@ -1,30 +1,21 @@
 <script>
-    import {tick} from "svelte";
-    import {generateKML, KmlGenerator} from '../kml.js';
-    import DownloadButton from "./DownloadButton.svelte";
-    export let kmlOptions;
+    import {KmlGenerator} from '../kml.js';
 
+    export let kmlOptions;
     let kml = "";
 
     function changeKml(node) {
         kml = KmlGenerator().render();
         return {
-            async update(kmlOptions) {
-                await tick();
+            update(kmlOptions) {
                 kml = KmlGenerator().render();
-            },
-            destroy() {
-                KmlGenerator().reset();
             }
         }
     }
 </script>
 
 <textarea use:changeKml={kmlOptions} class="text-monospace">{kml}</textarea>
-<div>
-    <DownloadButton label="Télécharger KML MapsMe" type="mapsme"/>
-    <DownloadButton label="Télécharger KML Avenza" type="avenza"/>
-</div>
+
 <style>
     textarea {
         width: 100%;
