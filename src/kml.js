c53b0ec14cde37476d4ad27ef46bbe7dfdd909c2
Eric Delord <edelord@gmail.com>
2020-05-21 17:01:30 +0200

    refactoring of kml bridge

--- src/kml.js
+++ src/kml.js
@@ -16,28 +16,34 @@ export const kmlDefaultOptions = {
     "ogimetColor": "40FF0000"
 };
 
+function skeleton (kmlGen, kmlOptions) {
+  kmlGen.addFolders(
+    {"name": "greatcircle", "enabled": kmlOptions.greatCircleDisplay, "color": kmlOptions.greatCircleColor},
+    {"name": "ogimet", "enabled": kmlOptions.ogimetDisplay, "color": kmlOptions.ogimetColor},
+    {
+        "name": "ralt",
+        "enabled": kmlOptions.alternateDisplay,
+        "color": kmlOptions.alternateColor,
+        "pinId": kmlOptions.alternatePin
+    },
+    {"name": "rmain", "color": kmlOptions.routeColor, "pinId": kmlOptions.routePin},
+    {"name": "rnat", "enabled": kmlOptions.natDisplay, "color": kmlOptions.natColor, "pinId": kmlOptions.natPin},
+    {
+        "name": "rnat_incomplete",
+        "enabled": kmlOptions.natDisplay,
+        "color": kmlOptions.natIncompleteColor,
+        "pinId": kmlOptions.natPin
+    }
+  );
+}
+
+/**
+ * This is a singleton definition which can be imported  even without lidojs loaded
+ */
 export const KmlGenerator = () => {
   if (!(window['kmlGen'])) {
     window['kmlGen'] = new editolido.KMLGenerator();
-    const kmlOptions = kmlDefaultOptions;
-    window['kmlGen'].addFolders(
-        {"name": "greatcircle", "enabled": kmlOptions.greatCircleDisplay, "color": kmlOptions.greatCircleColor},
-        {"name": "ogimet", "enabled": kmlOptions.ogimetDisplay, "color": kmlOptions.ogimetColor},
-        {
-            "name": "ralt",
-            "enabled": kmlOptions.alternateDisplay,
-            "color": kmlOptions.alternateColor,
-            "pinId": kmlOptions.alternatePin
-        },
-        {"name": "rmain", "color": kmlOptions.routeColor, "pinId": kmlOptions.routePin},
-        {"name": "rnat", "enabled": kmlOptions.natDisplay, "color": kmlOptions.natColor, "pinId": kmlOptions.natPin},
-        {
-            "name": "rnat_incomplete",
-            "enabled": kmlOptions.natDisplay,
-            "color": kmlOptions.natIncompleteColor,
-            "pinId": kmlOptions.natPin
-        }
-    );
+    skeleton(window['kmlGen'], kmlDefaultOptions);
   }
   return window['kmlGen'];
 }
@@ -65,9 +71,9 @@ export function updateKml(name, value) {
   }else if (name.endsWith('-pin')) {
       kmlGen.changeFolderPin(folder, value);
   }else if (name.endsWith('-color')) {
-      kmlGen.changeFolderColor(folder, value);
+      kmlGen.changeFolderColor(folder, value.toUpperCase());
   }else{
-      console.err(`ignoring kml update for ${name}`);
+      console.error(`ignoring kml update for ${name}`);
   }
 }
 /**
@@ -77,9 +83,10 @@ export function updateKml(name, value) {
  * @param options: {}
  * @returns {string|Source|ConcatSource}
  */
-export function generateKML(ofp) {
-    //let kmlOptions = {...kmlDefaultOptions, ...options};
+export function generateKML(ofp, options) {
+    let kmlOptions = {...kmlDefaultOptions, ...options};
     const kmlGen = KmlGenerator();
+    skeleton(kmlGen, kmlOptions); //perform reset also
     const description = ofp.description;
     kmlGen.setName(description);
     const routeName = `${ofp.infos.departure}-${ofp.infos.destination}`;
