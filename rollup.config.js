045b4de9bd412a442599e47cc8816c9a1ef7861b
Eric Delord <edelord@gmail.com>
2020-05-25 18:48:09 +0200

    dependancy cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
-import scss from 'rollup-plugin-scss'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -16,7 +15,6 @@ export default {
     file: 'public/build/bundle.js'
   },
   plugins: [
-    scss(), // will output compiled styles to output.css
     svelte({
       // enable run-time checks when not in production
       dev: !production,
