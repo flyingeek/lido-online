47f75cb22a94eed212736cf875e04e12859a6343
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-03-18 17:45:16 +0100

    TakeOffInput css tweaks

--- src/components/SimulatorPlayer.svelte
+++ src/components/SimulatorPlayer.svelte
@@ -10,7 +10,7 @@
 
 <!-- <input bind:value={$fl} type="range" min=200 max=450 step=1/>{$fl} -->
 <!-- <input bind:value={$grametPosition} type="range" min=0 max=100 step=1/> -->
-<label for="{name}" on:click={toggle}>{($simulate >= 0) ? '◼︎' :'►'}
+<label for="{name}" on:click={toggle}>{($simulate >= 0) ? '◼︎' :'▶︎'}
 <input bind:this={input} name="{name}" type="checkbox" checked={$simulate>=0} on:change={toggle}>
 </label>
 <style>
@@ -20,5 +20,6 @@
     label{
         margin-right: 1rem;
         font-size: large;
+        font-family: monospace;
     }
 </style>
--- src/components/TakeOffInput.svelte
+++ src/components/TakeOffInput.svelte
@@ -30,13 +30,16 @@
             newTakeOff = new Date(newTakeOff.getTime() - (1440 * 60000)); // -1 day
         }
         $takeOffTime = newTakeOff;
+        if (newTakeOff.toISOString() !== ofpTakeOff.toISOString()) {
+            e.target.classList.add("changed");
+        }else{
+            e.target.classList.remove("changed");
+        }
     };
 </script>
 
 <div>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Aircraft_take_off" x="0px" y="0px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve">
-        <head/><path d="M19.87,6.453c0.119,0.257,0.127,1.29-4.884,3.642l-4.913,2.306c-1.71,0.803-4.191,1.859-5.285,2.151  c-0.766,0.204-1.497-0.316-1.497-0.316s-2.206-1.975-2.792-2.419c-0.585-0.444-0.535-0.67,0.215-0.91  c0.467-0.149,3.13,0.493,4.265,0.78c1.339-0.8,2.562-1.486,3.141-1.798c-1.396-1.033-4.008-2.962-4.841-3.55  c-0.799-0.565,0.01-0.768,0.01-0.768c0.368-0.099,1.162-0.228,1.562-0.144c2.721,0.569,7.263,2.071,7.611,2.186  c0.832-0.436,2.128-1.092,2.922-1.465C17.459,5.174,19.711,6.111,19.87,6.453z"/>
-    </svg>
+    <svg><use xlink:href="#takeoff-symbol"/></svg>
     <label for="{name}">Heure de décollage</label><!-- displayed in ios popup -->
     <input id="{name}" name="{name}" type="time" required="required" on:change={changeTime} value="{hm2input(ofpTakeOff.getUTCHours(), ofpTakeOff.getUTCMinutes())}" />
 </div>
@@ -54,10 +57,11 @@
     }
     svg {
         width: 30px;
+        height: 30px;
         display: inline-block;
         margin-right: 2px;
         fill: rgba(0,0,0,.5);
-        margin-top: -2px;
+        margin-top: -1px;
     }
     input {
         background-color: var(--light);
@@ -67,6 +71,9 @@
         -webkit-appearance: none;
         -moz-appearance: none;
     }
+    div :global(input.changed){
+        color:rgba(0,0,0,.9);
+    }
     @media not all and (min-resolution:.001dpcm) { /* Safari hack */
         @media all{
             input { 
@@ -74,7 +81,7 @@
             }
         }
         div {
-            margin-right: 0;
+            margin-right: 6px; /* just enough place for the outline */
         }
     }
 
