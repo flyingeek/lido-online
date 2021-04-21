a6ce627cc0c8c1e543090f73345e7f11f3222f3e
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-04-21 15:41:51 +0200

    simplified projection selected, the automatic selection is in uppercase, all others in lowercase

--- src/components/MapProjectionSelect.svelte
+++ src/components/MapProjectionSelect.svelte
@@ -6,6 +6,7 @@
     export let name = 'map-style';
     export let ofp;
     export let selected = options[0];
+    let autoSelectedId = selected.id;
     const authorizedOptions = (!ofp.isFake) ? options : options.filter(o => (!o.id.startsWith('vb_') && o.id !== 'jb_theworld'));
 
     function isInBounds(option, dep, dest) {
@@ -24,44 +25,34 @@
         }
         return true;
     }
-    function mapContainsOfp(option) {
-        if (!ofp
-            ||ofp.isFake) return true;
-        if (option.id === 'jb_theworld'
-            ||option.id === 'mercator'
-            ||option.id.startsWith('ed_eqe_')) return true;
-        const dep = ofp.route.points[0];
-        const dest = ofp.route.points[ofp.route.points.length - 1];
-        if (option.id === 'jb_north') {
-            return (dest.latitude > 30 && dep.latitude > 30);
-        } else if (option.id === 'jb_south') {
-            if (dest.latitude > 30 && dep.latitude > 30) return false;
-        }
-        return isInBounds(option, dep, dest);
-    }
+
     onMount(() => {
         if (selected.id === options[0].id && ofp && !ofp.isFake) {
             const dep = ofp.route.points[0];
             const dest = ofp.route.points[ofp.route.points.length - 1];
             const cbMapOption = options.find(o => o.id === 'vb_2020');
+            const northOption = options.find(o => o.id === 'jb_north');
             if (isInBounds(cbMapOption, dep, dest)){
                 selected = cbMapOption;
             }else if (dep.distanceTo(dest, editolido.rad_to_nm) <=1500){
                 //pass (mercator)
             }else if (dest.latitude > 30 && dep.latitude > 30){
-                selected = options.find(o => o.id === 'jb_north');
+                selected = northOption;
             }else if (dest.longitude < -80 && dep.longitude < -80) {
                 selected = options.find(o => o.id === 'jb_pacific');
+            }else if ((dest.latitude > 30 || dep.latitude > 30) && isInBounds(northOption, dep, dest)) {
+                selected = northOption;
             }else{
                 selected = options.find(o => o.id === 'jb_south');
             }
         }
+        autoSelectedId = selected.id;
     });
 </script>
 <!-- svelte-ignore a11y-no-onchange -->
 <select name="{name}" bind:value={selected} class="form-control form-control-sm" on:change use:blurAction>
     {#each authorizedOptions as option, index}
-    <option value="{option}" selected={option.id === selected.id}>{(mapContainsOfp(option)) ? `${option.label.toUpperCase()}`: option.label}</option>
+    <option value="{option}" selected={option.id === selected.id}>{(option.id === autoSelectedId) ? `${option.label.toUpperCase()}`: option.label}</option>
     {/each}
 </select>
 
