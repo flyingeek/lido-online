be7d824dcd7ef25ab79e76112af9a02be1203d47
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-08-18 17:28:30 +0200

    slightly reduced line-with for ogimet and great-circle layers. (nicer scaling when zoomin/dezooming general layout)

--- src/components/mapboxgl/layers/great-circle.js
+++ src/components/mapboxgl/layers/great-circle.js
@@ -1,7 +1,8 @@
 /* global editolido */
-import {addLine, changeDisplayGeneric, changeLineGeneric, computeLineWidthSize} from '../utils';
+import {addLine, changeDisplayGeneric, changeLineGeneric, changeLineWidthGeneric, computeLineWidthSize} from '../utils';
 
 const folder = 'great-circle';
+const lineWidthGreatCircle = 1.5;
 
 const addGreatCircle = (data) => {
     const {ofp, kmlOptions, mapData, map, mapOptions} = data;
@@ -11,7 +12,7 @@ const addGreatCircle = (data) => {
     const routeName = `${ofp.infos.depICAO}-${ofp.infos.destICAO}`;
     const route = ofp.route;
     const greatCircle = new editolido.Route([route.points[0], route.points[route.points.length - 1]]).split(300, {"name": `Ortho ${routeName}`});
-    const lineWidth = computeLineWidthSize(kmlOptions['lineWidthChange']);
+    const lineWidth = computeLineWidthSize(kmlOptions['lineWidthChange'], lineWidthGreatCircle);
     addLine(map, folder, greatCircle.points, affineAndClip, kmlOptions.greatCircleColor, kmlOptions.greatCircleDisplay, lineWidth);
 }
 
@@ -20,4 +21,5 @@ export default {
     hide: changeDisplayGeneric.bind(null, folder, false),
     add: addGreatCircle,
     changeLine: changeLineGeneric.bind(null, folder),
+    changeLineWidth: (data) => changeLineWidthGeneric(folder, data, lineWidthGreatCircle)
 }
--- src/components/mapboxgl/layers/ogimet.js
+++ src/components/mapboxgl/layers/ogimet.js
@@ -1,13 +1,14 @@
-import {addLine, changeDisplayGeneric, changeLineGeneric, computeLineWidthSize} from '../utils';
+import {addLine, changeDisplayGeneric, changeLineGeneric, changeLineWidthGeneric, computeLineWidthSize} from '../utils';
 
 const folder = 'ogimet';
+const lineWidthOgimet = 1.5;
 
 const addOgimet = (data) => {
     const {map, ofp, kmlOptions, mapData, mapOptions} = data;
     if (mapOptions.id.startsWith('vb_')) return; // not relevant on this map
     if (!ofp) return;
     const {affineAndClip} = mapData;
-    const lineWidth = computeLineWidthSize(kmlOptions['lineWidthChange']);
+    const lineWidth = computeLineWidthSize(kmlOptions['lineWidthChange'], lineWidthOgimet);
     addLine(map, folder, ofp.ogimetData.route.points, affineAndClip, kmlOptions.ogimetColor, kmlOptions.ogimetDisplay, lineWidth);
 }
 
@@ -16,4 +17,5 @@ export default {
     hide: changeDisplayGeneric.bind(null, folder, false),
     add: addOgimet,
     changeLine: changeLineGeneric.bind(null, folder),
+    changeLineWidth: (data) => changeLineWidthGeneric(folder, data, lineWidthOgimet)
 }
