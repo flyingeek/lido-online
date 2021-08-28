e67c73183f2210f20c6b579b3257e0290695ed51
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-08-28 17:31:16 +0200

    changed Home Page

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -4,82 +4,86 @@
 <Logo/>
 
 <div class="row">
-  <div class=" col-12 col-sm-6 col-md-6 col-xl-3"><div class="card feature avenza">
-    <div class="card-header">COMPATIBLE OFFLINE</div>
-    <div class="card-body">
-      <p class="card-text">L'OFP ne quitte pas votre iPad ou votre ordinateur. Fonctionne avec les dernières technologies: PWA, Service Worker, fully responsive, cartes WebGL...</p>
-    </div>
-  </div></div>
-  <div class="col-12 col-sm-6 col-md-6 col-xl-3"><div class="card feature mapsme">
-    <div class="card-header">Multiples Projections</div>
-    <div class="card-body">
-      <p class="card-text">Des cartes LAMBERT conformes, un ATLAS politique, un ATLAS physique, une carte VFR et une MERCATOR avec 12 niveaux de zoom. Export en format KML vers Mapsme, Avenza ou Google Earth</p>
-    </div>
-  </div></div>
-  <div class="col-12 col-sm-6 col-md-6 col-xl-3"><div class="card feature ogimet">
-    <div class="card-header">Compatible avec Ogimet</div>
-    <div class="card-body">
-      <p class="card-text">Calcule une route pour ogimet et affiche le GRAMET qui est un météogramme combinant l'espace et le temps</p>
-    </div>
-  </div></div>
-  <div class="col-12 col-sm-6 col-md-6 col-xl-3"><div class="card feature shortcuts">
-    <div class="card-header">Compatible Raccourcis</div>
-    <div class="card-body">
-      <p class="card-text">Avec l'app Raccourcis, automatisez en récupérant le KML, le Gramet, ou une route Lido optimisée</p>
-    </div>
-  </div></div>
-</div>
-<div class="row">
-  <div class="col">
-    <div class="usage card">
+  <div class="col-12 col-lg-4">
+    <div class="card feature usage">
+      <div class="card-header">Facile à utiliser</div>
       <div class="card-body">
-        <h5 class="card-title">Utilisation&ThinSpace;:</h5>
-        <p class="card-text">Dans Pilot Mission, onglet «Dossier de vol», affichez le PDF nommé "Dossier de vol OFP". Sur la gauche, cliquez sur le carré avec une flèche vers le haut. Choisissez «Enregistrer dans Fichiers». Dans l'app, c'est cet OFP qu'il faut ensuite sélectionner.</p>
+        <p class="card-text">Dans <b>Pilot Mission</b>, onglet <b>Dossier de vol</b>, affichez le PDF nommé <b>Dossier de vol OFP</b>. Sur la gauche, cliquez sur le carré avec une flèche vers le haut. Choisissez <b>Enregistrer dans Fichiers</b>. Dans l'app, c'est cet OFP qu'il faut sélectionner.</p>
       </div>
     </div>
   </div>
+  <div class="col-12 col-lg-4"><div class="card feature offline">
+    <div class="card-header">RAPIDE ET OFFLINE</div>
+    <div class="card-body">
+      <p class="card-text">L'OFP ne quitte pas votre iPad ou votre ordinateur. Conçu pour le PilotPad, peut s'utiliser en vol sans réseau et fonctionne avec les dernières technologies: PWA, Service Worker, fully responsive, cartes WebGL...</p>
+    </div>
+  </div></div>
+  <div class="col-12 col-lg-4"><div class="card feature maps">
+    <div class="card-header">Multiples Projections</div>
+    <div class="card-body">
+      <ul class="card-text">
+        <li>trois LAMBERT conformes</li>
+        <li>un ATLAS POLITIQUE</li>
+        <li>un ATLAS PHYSIQUE</li>
+        <li>une carte VFR</li>
+        <li>une MERCATOR (zoom x 12)</li>
+      </ul>
+      <p class="card-text">Export KML compatible Mapsme, Avenza et Google Earth.</p>
+    </div>
+  </div></div>
 </div>
+
 <style>
-.usage {
+/* .usage {
   margin: 0 15px;
-  background-color: rgba(var(--bs-light-rgb), 0.9);
-  color: var(--bs-dark);
-}
-.feature{
   color: white;
   background-color: rgba(0, 0, 0, 0.2);
-  margin: 15px;
-  height: 100%;
+} */
+.usage .card-header {
+  background-color: var(--bs-green);
+}
+.feature{
+  color: black;
+  background-color: rgba(255, 255, 255, 0.8);
+  border: none;
+  margin: 1rem 0;
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  font-size: 108%;
 }
 .feature .card-header {
   font-variant-caps: all-small-caps;
   font-weight: bold;
   color: white;
 }
-.feature.shortcuts .card-header {
+.feature.offline  .card-header{
   background-color: var(--bs-purple);
 }
-.feature.mapsme .card-header {
+/* .feature.mapsme .card-header {
   background-color: var(--bs-green);
-}
-.feature.avenza .card-header {
+} */
+.feature.maps .card-header {
   background-color: var(--bs-pink);
 }
-.feature.ogimet .card-header {
+/* .feature.ogimet .card-header {
   background-color: var(--bs-yellow);
-}
+} */
 .row {
-  margin-bottom: 15px; /* for no ofp prompt */
-  padding: 0 10px 10px 10px;
+  margin-bottom: 1rem; /* for no ofp prompt */
+  padding: 0 1rem 1rem 1rem;
 }
-@media (min-width: 576px) and (max-width: 1199.8px) {
-  .col-sm-6, .col-md-6 {
-      margin-bottom: 15px;
+ul {
+  list-style: square;
+}
+b, li {
+  white-space: nowrap;
+}
+@media (min-width: 992px){
+  .row {
+    margin-top: 2rem;
   }
-} 
-@media (min-width: 1200px){
-  .usage {
-    margin-top: 5rem;
+  .feature {
+    height: 100%;
   }
-} 
+}
 </style>
--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,28 +1,42 @@
 <script>
     import {shareAppLink} from './utils';
 </script>
-<div>
-    <h1><strong class="d-none">OFP2MAP </strong>Cartographie pour l'OFP</h1>
-    <img src="svg/Air_France_Logo.svg" alt="logo Air France">
-    {#if (navigator.standalone === true || 'process.env.NODE_ENV' === '"development"') && navigator.share}
-        <button class="btn btn-link" on:click={shareAppLink}>Partager l'App</button>
-    {:else}
-        &nbsp;
-    {/if}
+<div class="row">
+    <div class="col-12 col-lg-8">
+        <h1><strong class="d-none">OFP2MAP </strong>Cartographie pour l'OFP</h1>
+        <img src="svg/Air_France_Logo.svg" alt="logo Air France">
+        {#if (navigator.standalone === true || 'process.env.NODE_ENV' === '"development"') && navigator.share}
+            <button class="btn btn-link" on:click={shareAppLink}>Partager l'App</button>
+        {:else}
+            &nbsp;
+        {/if}
+    </div>
 </div>
 <style>
     div {
+        padding: 0 2rem;
+    }
+    div>div {
         text-align: center;
         background-color: rgba(0, 0, 0, 0.2);
-        padding: 1em;
-        margin: 1.2em auto 1em auto;
+        border-radius: 8px;
         font-variant-caps: all-small-caps;
         color: white;
-        min-width: 50vw;
+        display: flex;
+        padding: 0.5rem;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 1rem;
+        margin-bottom: 15px;
     }
     img{
-        width: 40vw;
+        width: 100%;
         margin: 0 auto;
         display: block;
+    }
+    @media (min-width: 992px){
+        div>div {
+            margin: 3rem auto;
+        }
     }
 </style>
