a1be07f3cc57079b26c4906e01223d3a945a4e9b
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-09-25 17:51:09 +0200

    rebuild gramet thumbnail on orientation change

--- src/components/GrametTrigger.svelte
+++ src/components/GrametTrigger.svelte
@@ -41,6 +41,7 @@
         }
     });
 </script>
+<svelte:window on:orientationchange={reload}/>
 {#if $grametStatus !== 'error' && $grametStatus !== 'reload'}
     <div class="gramet-thumbnail" class:open={$showGramet} use:grametThumbAction={{ofp: $ofp, pos: $position.gramet, fl: $position.fl}} on:click={toggleGramet}>
         <svg id="gt-plane" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
