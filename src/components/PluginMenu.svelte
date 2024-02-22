6062d64bb409c7e7a684b80acc6b1835a2b35770
Eric Delord <04-ete.elit@icloud.com>
2024-02-22 13:56:25 +0100

    added turbli to the plugin menu

--- src/components/PluginMenu.svelte
+++ src/components/PluginMenu.svelte
@@ -73,8 +73,14 @@
     <li><a class="text-reset text-decoration-none" href="https://fr.flightaware.com/live/flight/{$ofp.infos.aircraftRegistration.replace('-','')}" on:click={close} target="_blank">
         <span class="icon">
             <img src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAgICAgJCAkKCgkNDgwODRMREBARExwUFhQWFBwrGx8bGx8bKyYuJSMlLiZENS8vNUROQj5CTl9VVV93cXecnNEBCAgICAkICQoKCQ0ODA4NExEQEBETHBQWFBYUHCsbHxsbHxsrJi4lIyUuJkQ1Ly81RE5CPkJOX1VVX3dxd5yc0f/CABEIABQAFAMBIgACEQEDEQH/xAAwAAEAAgMAAAAAAAAAAAAAAAAAAgYDBQcBAAIDAQAAAAAAAAAAAAAAAAABAgMEBf/aAAwDAQACEAMQAAAA4ntoWnpZqOLYYg0Af//EACcQAAMAAgEBBgcAAAAAAAAAAAECAwQRAAUSExQhIjEWICMyQXFy/9oACAEBAAE/AACeDo2XTpkuowU2ibtB+wpJnT3UH+wdg8vC2PVpWk86LraupRhvz8w2jxr4OMGTGj3r/UXxFdj3+1pzB9BX8Ek8+OM89L61BH8M+VkQeK4yiIQKxanqTR23LXtej1rV3djtmZizH9k/J//EABwRAAEDBQAAAAAAAAAAAAAAAAIAAREDEBIhMf/aAAgBAgEBPwAyqaxFNMNPb//EABkRAQACAwAAAAAAAAAAAAAAAAEAAhARQf/aAAgBAwEBPwAKdY6z/9k=" alt="FlightAware logo"/>
-        </span>FlightAware</a></li>
-    {#if (true || navigator && navigator.share)}
+        </span>FlightAware</a>
+    </li>
+    <li><a class="text-reset text-decoration-none" href="https://turbli.com/{$ofp.infos.depIATA}/{$ofp.infos.destIATA}/{$ofp.infos.ofpOUT.toISOString().split('T')[0]}/{$ofp.infos.flightNo.replace(/([A-Z]+)(0*)([1-9]\d*)/,'$1-$3')}" on:click={close} target="_blank">
+        <span class="icon">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IArs4c6QAAAU9JREFUOE+llL1SwkAQx3dBtLKysbGUMDa+gW8iJngZCi3ofAcrCp1x4EiY+Ci+gYVDgqWFjZWVDuaci1y8LHdJgJS53d/+9xNhza/TvTuYRdcfNjdck5ebO+54EE/9IfXfGChBbY+LJGQFhhHouLwnECYm9RRAoStAaVBVBgo9ce8PX6ZX79KvAKwDU8F0qK4yB5pg1IkqV+9tbyLSZnr0yv03K5CmpWB6YJPKDEjV2WDStuNHIl1852L/Vf51fAVYBitTqeq4FVAgpPOANfWZ3AqopyuhxpQBcZQEl33bLJqaov6ZgctIWRo9/pkEbL+qdkpdYbBJp2XHGqYJEAC385DdyDfHfxRi8ZXFU+nXHmxaAts81l29pyRkZ8ce7yPAgw7HnV2Ix92cs9FxUMBGaw9mo/Pq8yV3E6D86NgWoPLA6rXCn9ZpHF08l523X54wxBWFLBNqAAAAAElFTkSuQmCC" alt="turbli icon">
+        </span>turbli</a>
+    </li>
+    {#if (navigator && navigator.share)}
         <li><a class="text-reset text-decoration-none" href="#top" on:click|preventDefault={shareOFP}><span class="icon">🧩</span>Plugins</a></li>
     {/if}
 </menu>
