d0a9bd318f0ad0d8e6ee59117870b4d34897ad7f
Eric Delord <04-ete.elit@icloud.com>
2024-03-17 01:58:19 +0800

    adds a window resize listener when not in standalone mode

--- src/actions/mapResizeAction.js
+++ src/actions/mapResizeAction.js
@@ -1,21 +1,26 @@
+import { debounce } from "../components/utils";
 export default function mapResizeAction(node, map) {
     let currentMap = map;
     const resizeMap = () => {
         if (currentMap && document && document.visibilityState && document.visibilityState === 'visible') {
             currentMap.resize();
-            //console.log('map resized');
+            //console.trace('map resized');
         }
     };
-    const observer = new IntersectionObserver((entries) => { 
-        entries.forEach(entry => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
         if(entry.intersectionRatio===1){
-            resizeMap();
+          resizeMap();
         }
-    });
+      });
     }, {threshold: 1});
     observer.observe(node);
     document.addEventListener("visibilitychange", resizeMap, false);
     window.addEventListener("orientationchange", resizeMap);
+    const resizeMapDebounced = debounce(resizeMap, 500);
+    if (! navigator.standalone) {
+      window.addEventListener("resize", resizeMapDebounced);
+    }
 
     return {
         update(map) {
@@ -24,6 +29,7 @@ export default function mapResizeAction(node, map) {
         destroy() {
             observer.unobserve(node);
             observer.disconnect();
+            window.removeEventListener("resize", resizeMapDebounced);
             window.removeEventListener("orientationchange", resizeMap);
             document.removeEventListener("visibilitychange", resizeMap);
             if (currentMap) currentMap.remove();
